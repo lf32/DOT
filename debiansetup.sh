7e1ded3fd2cdfac1e377971ffd2d4b4eff583ba2
lf32 <lf32.dev@gmail.com>
2022-08-13 14:30:08 +0530

    Update debiansetup.sh

--- debiansetup.sh
+++ debiansetup.sh
@@ -6,7 +6,7 @@ sudo apt install \
     build-essential curl python3-dev python3-venv libssl-dev libpq-dev \
     bzip2 xz-utils zlib1g libxml2-dev libxslt1-dev libpopt0 \
     libgpgme11 libdevmapper1.02.1 libguestfs-tools \
-    git vim neovim flameshot zsh zsh-sytax-highlighting
+    git vim neovim flameshot zsh zsh-syntax-highlighting
 
 # setup gh cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -23,16 +23,19 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 bash ~/.vim_runtime/install_awesome_vimrc.sh
 
 # setup zsh
-curl -sO "https://github.com/lf32/DOT/raw/main/zsh/.zshenv"
-curl -sO "https://github.com/lf32/DOT/raw/main/zsh/.zshrc"
+curl -sO "https://raw.githubusercontent.com/lf32/DOT/main/zsh/.zshenv"
+curl -sO "https://raw.githubusercontent.com/lf32/DOT/main/zsh/.zshrc"
 chsh -s /bin/zsh
 
 # setup gitconfig
-curl -sO "https://github.com/lf32/DOT/raw/main/git/.gitconfig"
-curl -sO "https://github.com/lf32/DOT/raw/main/git/.gittemplate"
+curl -sO "https://raw.githubusercontent.com/lf32/DOT/main/git/.gitconfig"
+curl -sO "https://raw.githubusercontent.com/lf32/DOT/main/git/.gittemplate"
 
 # setup sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update && sudo apt-get install sublime-text -y
+
+
+
 
