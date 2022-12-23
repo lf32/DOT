5ce47ed8f5086d2f94ad066b358a872a4add0b84
Akhil Raj <lf32.dev@gmail.com>
2022-12-24 02:26:15 +0530

    Create surfbins.sh

---
+++ scripts/surfbins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if the user has specified a project to view
+if [ "$1" = "g" ]; then
+  lynx "https://gtfobins.github.io/"
+elif [ "$1" = "l" ]; then
+  lynx "https://lolbas-project.github.io/#"
+else
+  echo "Usage: view-project [gtfobins|lolbas]"
+fi
+
+# To use it
+# chmod +x surfbins.sh
+# surfbins g|l
